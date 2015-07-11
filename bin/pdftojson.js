9987e8a78bc624f757bf08d9c355cab03cd926cb
MrOrz <johnsonliang7@gmail.com>
2015-07-11 14:12:58 +0800

    Fix pdftojson yargs setup

--- bin/pdftojson.js
+++ bin/pdftojson.js
@@ -4,15 +4,16 @@ var fs = require('fs'),
     pdftojson = require('../lib'),
     argv = require('yargs')
       .usage('Usage: $0 [options] pdfFile')
-      .boolean(['sort'])
+      .boolean('sort')
       .describe('sort', 'Ignores pdftotext\'s layout unfolding and sort the words according to their coordinates.')
-      .default('output', null, 'Output JSON file name')
+      .default('output', null)
+      .describe('output', 'Output JSON file name')
       .alias('output', 'o')
-      .default('cmd', 'Command line options to pass to `pdftotext`. Note: `-bbox` is enforced.')
+      .default('cmd', '')
+      .describe('cmd', 'Command line options to pass to `pdftotext`. Note: `-bbox` is enforced.')
       .alias('cmd', 'c')
       .example('$0 -c "-f 3 -l 6" YourPDF.pdf', 'Only process page 3 ~ 6 of YourPDF.pdf.')
       .demand(1, 1)
-      .epilog('Additional params will be passed to pdftojson.')
       .argv,
 
     pdfFileName = argv._[0],
